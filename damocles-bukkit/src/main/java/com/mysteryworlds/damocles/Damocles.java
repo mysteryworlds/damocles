65dc1ea30a0b1ad0f7a6a12d621ab35d7a4788f0
Felix Klauke <info@felix-klauke.de>
2019-12-30 18:34:47 +0100

    Add spigot bootstrapping

---
+++ damocles-bukkit/src/main/java/com/mysteryworlds/damocles/Damocles.java
@@ -0,0 +1,15 @@
+package com.mysteryworlds.damocles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Damocles extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    super.onEnable();
+  }
+
+  @Override
+  public void onDisable() {
+    super.onDisable();
+  }
+}
